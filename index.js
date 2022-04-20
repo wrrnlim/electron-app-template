895c122622d736fa949a75b8e818336e0bcf0177
Warren Lim <85139891+wrrnlim@users.noreply.github.com>
2022-04-20 12:55:18 -0600

    Add linting and fix problems

--- index.js
+++ index.js
@@ -1,170 +1,175 @@
 /**
  * Electron related code
  */
+// eslint-disable-next-line import/no-extraneous-dependencies
 const electron = require('electron');
-const { app, BrowserWindow, ipcMain, shell, Menu } = electron; // ES6 destructuring -> equivalent to app = electron.app etc
-const { autoUpdater } = require("electron-updater");
 
-const debug = /--debug/.test(process.argv[2])
+const {
+  app, BrowserWindow, ipcMain, shell, Menu,
+} = electron; // ES6 destructuring -> equivalent to app = electron.app etc
+const { autoUpdater } = require('electron-updater');
 
-let mainWindow, data, updateWindow;
+const debug = /--debug/.test(process.argv[2]);
+
+let mainWindow;
+let updateWindow;
 const version = `v${app.getVersion()}`;
+// eslint-disable-next-line no-console
 console.log(`Starting ${version}`);
-
-/* Menus */
-const menuTemplate = [ // TODO change menu items
-    {
-        label: 'File',
-        submenu: [
-            {
-                label: 'Exit',
-                accelerator: 'CmdOrCtrl+Q',
-                role: 'quit'
-            }
-        ],
-    },
-    {
-        label: 'View',
-        submenu: [
-            {
-                label: 'Reload',
-                accelerator: 'CmdOrCtrl+R',
-                role: 'reload'
-
-            },
-            { type: 'separator' },
-            {
-                label: 'Actual Size',
-                accelerator: 'CmdOrCtrl+0',
-                role: 'resetZoom'
-            },
-            {
-                label: 'Zoom In',
-                accelerator: 'CmdOrCtrl+=',
-                role: 'zoomIn'
-            },
-            {
-                label: 'Zoom Out',
-                accelerator: 'CmdOrCtrl+-',
-                role: 'zoomOut'
-            },
-            {
-                label: 'Minimize',
-                accelerator: 'CmdOrCtrl+M',
-                role: 'minimize'
-            },
-        ]
-    }, {
-        label: 'Help',
-        submenu: [
-            { 
-                label: 'About',
-                click: () => {
-                    shell.openExternal('https://github.com/wrrnlim/electron-app-template') // TODO change URL
-                }
-            },
-            {
-                label: 'Report an Issue',
-                click: () => {
-                    shell.openExternal('https://github.com/wrrnlim/electron-app-template/issues/new/choose') // TODO change URL
-                }
-            },
-            {
-                label: 'Open Developer Tools',
-                accelerator: 'CmdOrCtrl+Shift+I',
-                role: 'toggleDevTools'
-            },
-            { type: 'separator' },
-            {
-                label: 'App Version: ' + version,
-                enabled: false
-            },
-            {
-                label: 'Check for Updates',
-                click: () => {
-                    createUpdateWindow();
-                    autoUpdater.checkForUpdatesAndNotify();
-                }
-            },
-        ]
-    }
-]
-
-/**
- * Create main window - Event based programming syntax 
- */
-app.on('ready', () => {
-    mainWindow = new BrowserWindow({
-        title: 'Electron App Template', // TODO change window title
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-            enableRemoteModule: true,
-        },
-        icon: './assets/img/icon.ico' // TODO change icon path
-    });
-
-    /* Open links in external browser */
-    mainWindow.webContents.setWindowOpenHandler(({ url }) => {
-        shell.openExternal(url);
-        return { action: 'deny' };
-    });
-
-    /* Load index.html */
-    mainWindow.loadFile('src/index.html');
-
-    /* Open Dev Tools if --debug flag passed */
-    if (debug) {
-      mainWindow.webContents.openDevTools()
-      // add debug menu items if needed
-    }
-
-    /* Menus */
-    const menu = Menu.buildFromTemplate(menuTemplate);
-    Menu.setApplicationMenu(menu);
-});
 
 /**
  * Create the update window
  */
 function createUpdateWindow() {
-    if (!updateWindow) { // Prevent update window from being created multiple times
-        updateWindow = new BrowserWindow({
-            width: 400,
-            height: 250,
-            title: 'Updater',
-            parent: mainWindow,
-            webPreferences: {
-                nodeIntegration: true,
-                contextIsolation: false,
-                enableRemoteModule: true,
-            },
-            icon: './assets/icons/icon.ico' // TODO change icon path
-        });
-        updateWindow.loadFile('src/update.html');
-        /* Wait for contents to load then set status */
-        updateWindow.webContents.on('did-finish-load', () => {
-            updateWindow.webContents.send('version', version);
-        });
-    }
-    else {
-        updateWindow.focus();
-    }
-    updateWindow.on('closed', () => { // set updateWindow to null so it can be created again
-        updateWindow = null;
+  if (!updateWindow) { // Prevent update window from being created multiple times
+    updateWindow = new BrowserWindow({
+      width: 400,
+      height: 250,
+      title: 'Updater',
+      parent: mainWindow,
+      webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+        enableRemoteModule: true,
+      },
+      icon: './assets/icons/icon.ico', // TODO change icon path
     });
+    updateWindow.loadFile('src/update.html');
+    /* Wait for contents to load then set status */
+    updateWindow.webContents.on('did-finish-load', () => {
+      updateWindow.webContents.send('version', version);
+    });
+  } else {
+    updateWindow.focus();
+  }
+  updateWindow.on('closed', () => { // set updateWindow to null so it can be created again
+    updateWindow = null;
+  });
 }
+
+/* Menus */
+const menuTemplate = [ // TODO change menu items
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Exit',
+        accelerator: 'CmdOrCtrl+Q',
+        role: 'quit',
+      },
+    ],
+  },
+  {
+    label: 'View',
+    submenu: [
+      {
+        label: 'Reload',
+        accelerator: 'CmdOrCtrl+R',
+        role: 'reload',
+
+      },
+      { type: 'separator' },
+      {
+        label: 'Actual Size',
+        accelerator: 'CmdOrCtrl+0',
+        role: 'resetZoom',
+      },
+      {
+        label: 'Zoom In',
+        accelerator: 'CmdOrCtrl+=',
+        role: 'zoomIn',
+      },
+      {
+        label: 'Zoom Out',
+        accelerator: 'CmdOrCtrl+-',
+        role: 'zoomOut',
+      },
+      {
+        label: 'Minimize',
+        accelerator: 'CmdOrCtrl+M',
+        role: 'minimize',
+      },
+    ],
+  }, {
+    label: 'Help',
+    submenu: [
+      {
+        label: 'About',
+        click: () => {
+          shell.openExternal('https://github.com/wrrnlim/electron-app-template'); // TODO change URL
+        },
+      },
+      {
+        label: 'Report an Issue',
+        click: () => {
+          shell.openExternal('https://github.com/wrrnlim/electron-app-template/issues/new/choose'); // TODO change URL
+        },
+      },
+      {
+        label: 'Open Developer Tools',
+        accelerator: 'CmdOrCtrl+Shift+I',
+        role: 'toggleDevTools',
+      },
+      { type: 'separator' },
+      {
+        label: `App Version: ${version}`,
+        enabled: false,
+      },
+      {
+        label: 'Check for Updates',
+        click: () => {
+          createUpdateWindow();
+          autoUpdater.checkForUpdatesAndNotify();
+        },
+      },
+    ],
+  },
+];
+
+/**
+ * Create main window - Event based programming syntax
+ */
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    title: 'Electron App Template', // TODO change window title
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
+    icon: './assets/img/icon.ico', // TODO change icon path
+  });
+
+  /* Open links in external browser */
+  mainWindow.webContents.setWindowOpenHandler(({ url }) => {
+    shell.openExternal(url);
+    return { action: 'deny' };
+  });
+
+  /* Load index.html */
+  mainWindow.loadFile('src/index.html');
+
+  /* Open Dev Tools if --debug flag passed */
+  if (debug) {
+    mainWindow.webContents.openDevTools();
+    // add debug menu items if needed
+  }
+
+  /* Menus */
+  const menu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(menu);
+});
 
 /**
  * Show status on update window
  */
 function showStatus(status) {
-    updateWindow.webContents.send('updater-data', status);
+  updateWindow.webContents.send('updater-data', status);
 }
 
-app.on("window-all-closed", () => {
+app.on('window-all-closed', () => {
   app.quit();
 });
 
@@ -172,33 +177,33 @@ app.on("window-all-closed", () => {
  * Auto updater
  */
 autoUpdater.on('checking-for-update', () => {
-    showStatus('Checking for updates...');
-})
-autoUpdater.on('update-available', (info) => {
-    showStatus('Update found! Download will begin shortly.');
-})
-autoUpdater.on('update-not-available', (info) => {
-    showStatus('You have the newest version!');
-})
+  showStatus('Checking for updates...');
+});
+autoUpdater.on('update-available', () => {
+  showStatus('Update found! Download will begin shortly.');
+});
+autoUpdater.on('update-not-available', () => {
+  showStatus('You have the newest version!');
+});
 autoUpdater.on('error', (err) => {
-    showStatus('Error in auto-updater. ' + err);
-})
+  showStatus(`Error in auto-updater. ${err}`);
+});
 autoUpdater.on('download-progress', (progressObj) => {
-    showStatus('Downloading: ' + progressObj.percent.toFixed(2) + '%');
-})
-autoUpdater.on('update-downloaded', (info) => {
-    updateWindow.webContents.send('show-buttons');
-    showStatus('Update downloaded. Restart app to apply update.');
+  showStatus(`Downloading: ${progressObj.percent.toFixed(2)}%`);
+});
+autoUpdater.on('update-downloaded', () => {
+  updateWindow.webContents.send('show-buttons');
+  showStatus('Update downloaded. Restart app to apply update.');
 });
 
 /**
  * IPC Listeners
  */
 ipcMain.on('restart-app', () => {
-    autoUpdater.quitAndInstall(isSilent = true, isForceRunAfter = true);
+  // eslint-disable-next-line no-undef
+  autoUpdater.quitAndInstall(isSilent = true, isForceRunAfter = true);
 });
 
 ipcMain.on('close-update-window', () => {
-    updateWindow.close();
+  updateWindow.close();
 });
-
--- src/update.js
+++ src/update.js
@@ -1,25 +1,26 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 const { ipcRenderer } = require('electron');
 
 ipcRenderer.on('updater-data', (event, status) => {
-    document.getElementById('status_text').innerHTML = status;
+  document.getElementById('status_text').innerHTML = status;
 });
 
 ipcRenderer.on('version', (event, version) => {
-    document.getElementById('version_text').innerHTML = version;
+  document.getElementById('version_text').innerHTML = version;
 });
 
-ipcRenderer.on('show-buttons', (event) => {
-    document.getElementById('restartBtn').hidden = false;
-    document.getElementById('laterBtn').hidden = false;
+ipcRenderer.on('show-buttons', () => {
+  document.getElementById('restartBtn').hidden = false;
+  document.getElementById('laterBtn').hidden = false;
 });
 
 document.getElementById('restartBtn').addEventListener('click', () => {
-    document.getElementById('status_text').innerHTML = 'Restarting app...please wait';
-    document.getElementById('restartBtn').disabled = true;
-    document.getElementById('laterBtn').disabled = true;
-    ipcRenderer.send('restart-app');
+  document.getElementById('status_text').innerHTML = 'Restarting app...please wait';
+  document.getElementById('restartBtn').disabled = true;
+  document.getElementById('laterBtn').disabled = true;
+  ipcRenderer.send('restart-app');
 });
 
 document.getElementById('laterBtn').addEventListener('click', () => {
-    ipcRenderer.send('close-update-window');
+  ipcRenderer.send('close-update-window');
 });
